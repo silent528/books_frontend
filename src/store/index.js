3a617df59199e695f096b5aa4c5f59c489ecf053
xiawenqiang <406413214@qq.com>
2019-03-15 17:40:30 +0800

    手机端 初始化

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import xml2json from 'xmlstring2json'
+import { formatSlideList, formatNewsList, formatTopicList } from '@/utils'
+import api from '@/utils/api'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
+})
+
+export default store
